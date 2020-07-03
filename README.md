40937ff9b9e8591d719ec4c4bb3903b5df64c5af
Mikko Jämiä <66477579+mikkojamG@users.noreply.github.com>
2020-07-03 13:16:35 +0300

    Merge latest gulp tasks
    * Add Eslint and husky
    
    * Edit gulfile
    
    * Edit package.json
    
    * Edit gulpfile
    
    * Remove and ignore dependencyGraph
    
    feat/finna-theme-migration (#4)
    
    * Gulp tasks and hierarchy modifications
    
    * Remove extra mockjax
    
    * Remove unnecessary build scripts
    
    * Remove unnecessary comment
    
    * Remove unnecessary whitespace
    
    * Check if patterns.less exists and create
    
    * Remove unnecessary config
    
    * Edit README
    
    * Edit README
    
    * Edit README
    
    * Change script naming
    
    * Add Gulp tasks descriptions
    
    Assets files under components (#5)
    
    Create symlink tasks
    
    Add .env
    
    Cleanup
    
    Update README
    
    Link component subfolders
    
    Force overwrite symlinks
    
    Add option to copy theme
    
    Remove linking from postinstall
    
    Create .gitkeep
    
    Fix patterns directory naming
    
    Set theme directory as a default
    
    Link sources as a default
    
    Edit readme
    
    Edit .gitignore
    
    Remove unnecessary overwrite setting
    
    Test for fetching bootstrap
    
    Add task for injecting Less components
    
    Use env variable
    
    Inject vufind
    
    Use shell for symlinks, rename _patterns
    
    Inject themes path

--- README.md
+++ README.md
@@ -1,25 +1,57 @@
-# Finna UI Component Library
-## Installation
-Copy the repository
+# Finna UI Component Library Proto
+## Cloning the repository
 ```
 git clone git@github.com:mikkojamG/ui-component-library-proto.git
 ```
-Install development dependencies and Pattern Lab.
+
+## Installation
+
+Running the script installs development dependencies, Pattern Lab and as a default creates symbolic link from the library source components to your working theme directory.
+
 ```
 yarn
 ```
+
 or
+
 ```
 npm install
 ```
 
+## Scripts
+
+### Development
+
 Start the development server with hot reloading
+
 ```
-yarn serve
+yarn dev
+```
+
+or
+
+```
+npm run dev
+```
+
+### Link theme
+Create a symbolic links between source components and working theme directory.
+```
+yarn link:theme
 ```
 or
 ```
-npm run serve
+npm run link:theme
+```
+
+### Copy theme
+Alternative to linking theme, copy source components to working theme.
+```
+yarn copy:theme
+```
+or
+```
+npm run copy:theme
 ```
 
 ## engine-phtml
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const config = require('./patternlab-config.json');
 
 const gulp = require('gulp');
 const less = require('gulp-less');
 const shell = require('gulp-shell');
 const browserSync = require('browser-sync');
+const path = require('path');
 
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
@@ -11,17 +13,22 @@ const minify = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
+const inject = require('gulp-inject');
 
-const cleanPublic = () => {
-  return gulp.src(`${config.paths.public.root}/*`).pipe(clean({force: true}));
-};
+// Helpers
+const cleanDir = (dir) => gulp.src(`${dir}/*`).pipe(clean({ force: true }));;
+
+// Tasks
+const cleanPublic = () => cleanDir(config.paths.public.root);
+gulp.task(cleanPublic);
 
 const patternLab = () => {
   return gulp
-    .src('.', {allowEmpty: true})
+    .src('.', { allowEmpty: true })
     .pipe(shell(['patternlab build --config ./patternlab-config.json']))
     .pipe(browserSync.stream());
 };
+gulp.task(patternLab);
 
 const styles = () => {
   const source = config.paths.source.styles;
@@ -32,42 +39,57 @@ const styles = () => {
     .pipe(less())
     .pipe(autoprefixer())
     .pipe(minify())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(dest))
     .pipe(browserSync.stream());
 };
+gulp.task(styles);
 
-const globalScripts = () => {
-  const source = config.paths.source.js;
+const bootstrap = () => {
+  const stylesheet = config.paths.source.styles;
+
+  return gulp.src(`${stylesheet}/vendor/finna.less`)
+    .pipe(inject(
+      gulp.src(process.env.THEMES_ROOT, { read: false }),
+      {
+        starttag: '/* Theme path starts */',
+        endtag: '/* Theme path ends */',
+        transform: (filePath) => {
+          return `@themePath: "../../..${filePath}";`
+        }
+      }
+    ))
+    .pipe(gulp.dest(`${stylesheet}/vendor`));
+};
+
+const scripts = () => {
+  const source = config.paths.source.root;
   const dest = config.paths.public.js;
 
   return gulp
-    .src(`${source}/*.js`)
+    .src([`${source}/js/finna.js`, `!${source}/js/vendor/*.js`, `${source}/components/**/*.js`])
     .pipe(concat('main.js'))
     .pipe(gulp.dest(dest))
     .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(dest))
     .pipe(browserSync.stream());
 };
+gulp.task(scripts);
 
-const componentScripts = () => {
+const vendorScripts = () => {
   const source = config.paths.source.js;
-  const dest = `${config.paths.public.js}/components`;
+  const dest = `${config.paths.public.js}/vendor`;
 
   return gulp
-    .src(`${source}/components/**/*.js`)
+    .src(`${source}/vendor/*.js`)
     .pipe(gulp.dest(dest))
     .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(dest))
     .pipe(browserSync.stream());
 };
-
-const defaultTask = gulp.series(
-  cleanPublic,
-  gulp.parallel(patternLab, styles, globalScripts, componentScripts)
-);
+gulp.task(vendorScripts);
 
 const watchTask = () => {
   browserSync.init({
@@ -75,18 +97,146 @@ const watchTask = () => {
       baseDir: config.paths.public.root
     },
     ghostMode: true,
-    open: 'external'
+    open: 'external',
+    snippetOptions: {
+      blacklist: ['/index.html', '/']
+    }
   });
 
   gulp.watch(`${config.paths.source.styles}/**/*.less`, styles);
   gulp.watch(`${config.paths.source.patterns}**/*.less`, styles);
 
-  gulp.watch(`${config.paths.source.js}/*.js`, globalScripts);
-  gulp.watch(`${config.paths.source.js}/components/**/*.js`, componentScripts);
+  gulp.watch(`${config.paths.source.js}/**/*.js`, scripts);
+  gulp.watch(`${config.paths.source.patterns}**/*.js`, scripts);
 
   gulp.watch(`${config.paths.source.patterns}**/*.phtml`, patternLab);
+  gulp.watch(`${config.paths.source.patterns}**/*.json`, patternLab);
 };
+gulp.task(watchTask);
 
-const serve = gulp.series(defaultTask, watchTask);
+const componentImports = () => {
+  const less = `${process.env.THEME_DIRECTORY}/less`
 
-exports.default = serve;
+  return gulp.src(`${less}/custom.less`)
+    .pipe(inject(gulp.src(`${less}/components/**/*.less`, { read: false }), {
+      starttag: '/* All custom less-code here */',
+      endtag: '/* Custom less-code ends */',
+      ignorePath: `/${process.env.THEME_DIRECTORY}/less/`,
+      addRootSlash: false,
+      transform: (filePath) => {
+        return `@import "${filePath}";`
+      }
+    }))
+    .pipe(gulp.dest(less))
+};
+gulp.task(componentImports);
+
+const symLinkPatterns = () => {
+  return gulp
+    .src('.', { allowEmpty: true })
+    .pipe(shell([
+      `cd ${process.env.THEME_DIRECTORY}/templates && ln -fs ../../../../ui-component-library-proto/source/components`
+    ]));
+};
+gulp.task(symLinkPatterns);
+
+const symLinkStyles = () => {
+  return gulp
+    .src('.', { allowEmpty: true })
+    .pipe(shell([`cd ${process.env.THEME_DIRECTORY}/less && ln -fs ../../../../ui-component-library-proto/source/components`]));
+};
+gulp.task(symLinkStyles);
+
+const symLinkScripts = () => {
+  return gulp
+    .src('.', { allowEmpty: true })
+    .pipe(shell([`cd ${process.env.THEME_DIRECTORY}/js && ln -fs ../../../../ui-component-library-proto/source/components`]));
+};
+gulp.task(symLinkScripts);
+
+const symLinkTheme = gulp.series(
+  symLinkPatterns,
+  symLinkStyles,
+  symLinkScripts,
+  componentImports
+);
+
+const copyPatterns = () => {
+  const source = config.paths.source.patterns;
+
+  return gulp
+    .src(`${source}**/*.phtml`)
+    .pipe(gulp.dest(`${process.env.THEME_DIRECTORY}/templates/components`));
+};
+gulp.task(copyPatterns);
+
+const copyStyles = () => {
+  const source = config.paths.source.patterns;
+
+  return gulp
+    .src(`${source}**/*.less`)
+    .pipe(gulp.dest(`${process.env.THEME_DIRECTORY}/less/components`));
+};
+gulp.task(copyStyles);
+
+const copyScripts = () => {
+  const source = config.paths.source.patterns;
+
+  return gulp
+    .src(`${source}**/*.js`)
+    .pipe(gulp.dest(`${process.env.THEME_DIRECTORY}/js/components`));
+};
+gulp.task(copyScripts);
+
+const copyTheme = gulp.series(copyPatterns, copyStyles, copyScripts, componentImports);
+
+const defaultTask = gulp.series(
+  cleanPublic,
+  gulp.parallel(patternLab, styles, scripts, vendorScripts)
+);
+
+const watch = gulp.series(defaultTask, watchTask);
+
+// Descriptions
+cleanPublic.description = "Clear all files under public directory";
+
+patternLab.description = "Build PatternLab to public directory";
+
+styles.description = "Convert and minify Less to CSS";
+
+scripts.description = "Build and uglify Javascript into main.js";
+
+vendorScripts.description = "Build and uglify vendor Javascript";
+
+watchTask.description = "Initialize BrowserSync instance and watch for changes";
+
+componentImports.description = "Inject component imports to dedicated files";
+
+symLinkPatterns.description = "Create patterns symbolic link";
+
+symLinkStyles.description = "Create styles symbolic link";
+
+symLinkScripts.description = "Create scripts symbolic link";
+
+copyPatterns.description = "Create patterns copy";
+
+copyStyles.description = "Create styles copy";
+
+copyScripts.description = "Create Javascript scripts copy";
+
+defaultTask.description = "Clear public directory and build patterns, CSS and Javascript from the source directory";
+
+watch.description = 'Build PatternLab from source files and watch for changes.';
+
+symLinkTheme.description = "Create symbolic link to working theme";
+
+copyTheme.description = "Create distributable copy to working theme";
+
+bootstrap.description = "Bootstrap Finna variables and fonts";
+
+// Exports
+exports.watch = watch;
+exports.symLinkTheme = symLinkTheme;
+exports.copyTheme = copyTheme;
+exports.bootstrap = bootstrap;
+

840ce516b5acda538da35783729dd7a69527326a
mikkojamG <mikko.jamia@gofore.com>
2020-08-31 12:33:09 +0300

    Cleanup

--- accessibility-log.js
+++ accessibility-log.js
@@ -2,13 +2,21 @@ const config = require('./patternlab-config.json');
 const pa11y = require('pa11y');
 const path = require('path');
 
-const patterns = [`${path.resolve(config.paths.public.patterns + 'atoms/index.html')}`, `${path.resolve(config.paths.public.patterns + 'molecules/index.html')}`, `${path.resolve(config.paths.public.patterns + 'organisms/index.html')}`]
+const patterns = [
+  `${path.resolve(config.paths.public.patterns + 'atoms/index.html')}`,
+  `${path.resolve(config.paths.public.patterns + 'molecules/index.html')}`,
+  `${path.resolve(config.paths.public.patterns + 'organisms/index.html')}`
+]
+
+const pa11yConfig = {
+  runners: ['htmlcs'],
+  timeout: 10000,
+  rootElement: '.pl-c-main'
+};
 
 const logAccessibilityIssues = async () => {
   try {
-    const promises = patterns.map(async (path) => await pa11y(path, {
-      runners: ['htmlcs'], timeout: 10000, rootElement: '.pl-c-main'
-    }));
+    const promises = patterns.map(async (path) => await pa11y(path, pa11yConfig));
 
     const results = await Promise.all(promises);
     const resultIssues = results.filter((result) => result.issues.length);

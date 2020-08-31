378c9b08a659c452ca8502be3e636900e5520dcb
mikkojamG <mikko.jamia@gofore.com>
2020-08-31 12:08:35 +0300

    Accessibility log script

---
+++ accessibility-log.js
@@ -0,0 +1,24 @@
+const config = require('./patternlab-config.json');
+const pa11y = require('pa11y');
+const path = require('path');
+
+const patterns = [`${path.resolve(config.paths.public.patterns + 'atoms/index.html')}`, `${path.resolve(config.paths.public.patterns + 'molecules/index.html')}`, `${path.resolve(config.paths.public.patterns + 'organisms/index.html')}`]
+
+const logAccessibilityIssues = async () => {
+  try {
+    const promises = patterns.map(async (path) => await pa11y(path, {
+      runners: ['htmlcs'], timeout: 10000, rootElement: '.pl-c-main'
+    }));
+
+    const results = await Promise.all(promises);
+    const resultIssues = results.filter((result) => result.issues.length);
+
+    const issues = resultIssues.map((result) => result.issues);
+
+    console.log(issues);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+logAccessibilityIssues();
